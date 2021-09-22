19963e40a45837b5c1449c94a01cd80b74d4425b
Sidra Kazmi <36916183+sidrakazmi@users.noreply.github.com>
2021-09-22 22:32:47 +0200

    secondupdate to ask

--- shop.js
+++ shop.js
@@ -1,6 +1,4 @@
 const cart = document.getElementById('cart');
-var items = document.getElementById('thing');
-const itemList = document.querySelector('#item-list tbody');
-const emptyCartBtn = document.getElementById('empty-cart');
+var items = document.getElementById('item');
 
 console.log(cart,items,itemList)
