69a0a6007f55d2c91c32525703e47ba24466cae4
Sidra Kazmi <36916183+sidrakazmi@users.noreply.github.com>
2021-09-24 13:20:22 +0200

    Update shop.js

--- shop.js
+++ shop.js
@@ -1,5 +1,6 @@
 const cart = document.getElementById('cart');
 var items = document.getElementById('item');
+var itemList = document.getElementById('item-list');
 
 console.log(cart,items,itemList)
 
