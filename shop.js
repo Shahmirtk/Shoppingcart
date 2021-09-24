595e12800553a9a9c4d19e5d51d10e8a6c11d94f
Sidra Kazmi <36916183+sidrakazmi@users.noreply.github.com>
2021-09-24 13:19:33 +0200

    Update shop.js

--- shop.js
+++ shop.js
@@ -2,3 +2,12 @@ const cart = document.getElementById('cart');
 var items = document.getElementById('item');
 
 console.log(cart,items,itemList)
+
+cartEventListeners ();
+function cartEventListeners () {
+  items.addEventListener('click', buyItem());
+}
+
+function buyItem() {
+
+}
